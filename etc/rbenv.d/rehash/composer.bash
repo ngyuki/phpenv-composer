b2c88c05980396d8b8cfa4cc31cf20cd704f38fd
ngyuki <ngyuki.jp@gmail.com>
2015-06-12 18:59:04 +0900

    fix #1

--- etc/rbenv.d/rehash/composer.bash
+++ etc/rbenv.d/rehash/composer.bash
@@ -6,8 +6,8 @@ cd "$RBENV_ROOT"
 make_shims composer versions/*/composer/vendor/bin/*
 
 # for system composer
-composer_cmd=$(RBENV_VERSION=system phpenv which composer)
-if [ -x "$composer_cmd" ]; then
+composer_cmd=$(RBENV_VERSION=system phpenv which composer 2>/dev/null ||:)
+if [ -n "$composer_cmd" -a -x "$composer_cmd" ]; then
   composer_home=$("$composer_cmd" config -g home)
   composer_bin_dir=$("$composer_cmd" config -g bin-dir)
   if [ -n "$composer_home" -a -n "$composer_bin_dir" -a -d "$composer_home/$composer_bin_dir" ]; then
--- etc/rbenv.d/which/composer.bash
+++ etc/rbenv.d/which/composer.bash
@@ -9,8 +9,8 @@ if [ "$RBENV_VERSION" != "system" ]; then
     RBENV_COMMAND_PATH="${RBENV_ROOT}/plugins/phpenv-composer/libexec/composer"
   fi
 elif [ "$RBENV_COMMAND" != "composer" ]; then
-  composer_cmd=$(RBENV_VERSION=system phpenv which composer)
-  if [ -x "$composer_cmd" ]; then
+  composer_cmd=$(RBENV_VERSION=system phpenv which composer 2>/dev/null ||:)
+  if [ -n "$composer_cmd" -a -x "$composer_cmd" ]; then
     composer_home=$("$composer_cmd" config -g home)
     composer_bin_dir=$("$composer_cmd" config -g bin-dir)
     if [ -n "$composer_home" -a -n "$composer_bin_dir" -a -x "$composer_home/$composer_bin_dir/$RBENV_COMMAND" ]; then
